ffb960b87d1f3b0011a81df82d4be98ea5fbea10
talha-husnain <92517278+talha-husnain@users.noreply.github.com>
2023-03-19 23:41:12 +0500

    Add files via upload

---
+++ src/Hello.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Hello = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+export default Hello;
---
+++ src/Pages/login/login.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./login.css";
+import axios from "axios";
+import img1 from "./img1.png";
+import { useNavigate } from "react-router-dom";
+
+const Login = (props) => {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const login = () => {
+    axios.post("http://localhost:9001/login", user).then((res) => {
+      alert(res.data.message);
+      props.setLoginUser(res.data.user);
+      navigate(`/`, { state: { email: user.email } });
+    });
+  };
+
+  return (
+    <div className="login" style={{alignItems:"start"}}>
+      <img style={{ maxWidth: "100%" }} src={img1} alt="thrift" />
+      <h1 className="h" style={{ marginBottom: "-0.5px", }}>
+        Enter your Email{" "}
+      </h1>
+
+      <input
+        type="text"
+        name="email"
+        value={user.email}
+        onChange={handleChange}
+        placeholder="Enter your Email"
+      ></input>
+      <h1 className="h"
+        style={{
+        //   fontSize: "110%",
+          marginBottom: "-0.5px",
+        //   marginLeft: "-70%",
+        }}
+      >
+        Enter Password{" "}
+      </h1>
+
+      <input
+        type="password"
+        name="password"
+        value={user.password}
+        onChange={handleChange}
+        placeholder="Enter your Password"
+      ></input>
+      <button className="button" onClick={login}>
+        Log in
+      </button>
+      <div>or</div>
+      <button className="button" onClick={() => navigate("/register")}>
+        Register
+      </button>
+    </div>
+  );
+};
+
+export default Login;
