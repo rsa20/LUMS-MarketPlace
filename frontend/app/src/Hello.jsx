cb5561c526b9aecff678f2ec4b3a8759fe6a637d
Abdul Moez <24100244@lums.edu.pk>
2023-03-25 22:21:10 +0500

    header search bar correction

--- frontend/app/src/Hello.jsx
+++ frontend/app/src/Hello.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Components/Footer/Footer';
+// import Header from './Header';
 
 export const Hello = () => {
   return (
     <div>
       <div>Hello</div>
+      {/* <Header /> */}
       <Footer />
     </div>
   );
