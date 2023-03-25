5ac004fad11de58d4ba690095f6e0b79a63f64a2
talha-husnain <92517278+talha-husnain@users.noreply.github.com>
2023-03-26 04:39:33 +0500

    Add files via upload

--- frontend/app/src/Pages/H2/Products.jsx
+++ frontend/app/src/Pages/H2/Products.jsx
@@ -1,40 +1,53 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeartPulse } from '@fortawesome/free-solid-svg-icons'
-
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
-import './product.css'
-const Product = ({ image, name, price,status }) => {
+import './product.css';
+import { useNavigate } from 'react-router-dom';
+
+const Product = ({ id, image, name, price, status }) => {
   const [isFavorite, setIsFavorite] = useState(false);
+  const navigate = useNavigate();
 
   const toggleFavorite = () => {
     setIsFavorite(!isFavorite);
   };
 
-  return (
-    <div className="product">
-      <img 
-    style={{ maxWidth: "100%", marginBottom: "2%", borderRadius: "5%" }}
+  const handleClick = async () => {
+    try {
+      const response = await fetch(`/api/products/${id}`);
+      const data = await response.json();
+      const { name, description, sellerId, price, status, images } = data;
+      const imageUrls = images.map((image) => image.url);
+      const productDetails = { name, description, sellerId, price, status, images: imageUrls };
+      navigate({
+        pathname: '/viewpost',
+        state: { productDetails }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
 
-      src={image} alt={name} />
+  return (
+    <div className="product" onClick={handleClick}>
+      <img style={{ maxWidth: "100%", marginBottom: "2%", borderRadius: "5%" }} src={image} alt={name} />
       <h3>{name}</h3>
       <div className='c-text'>
-
-      <span className = "ext">
-      <p style={{ marginLeft: "-30%", color: "#cc0000", fontWeight: "bold" }}>{price}</p>
-      <p style={{ marginLeft: "-70%", color: "#cc0000" }}>{status}</p>
+        <span className="ext">
+          <p style={{ marginLeft: "-30%", color: "#cc0000", fontWeight: "bold" }}>{price}</p>
+          <p style={{ marginLeft: "-70%", color: "#cc0000" }}>{status}</p>
         </span>
         <span className='icons'>
-
-                <FontAwesomeIcon
-                    icon={isFavorite ? faHeartPulse : faHeart}
-                    className={isFavorite ? 'favorite' : ''}
-                    onClick={toggleFavorite}
-                    style={{ color: 'red', fontSize: '2rem' }}
-
-                />
+          <FontAwesomeIcon
+            icon={isFavorite ? faHeartPulse : faHeart}
+            className={isFavorite ? 'favorite' : ''}
+            onClick={toggleFavorite}
+            style={{ color: 'red', fontSize: '2rem' }}
+          />
         </span>
-</div>
+      </div>
     </div>
   );
 };
