6f7997926bba4e0f0805622fba59eb4b6dea92c0
Abdul Moez <24100244@lums.edu.pk>
2023-04-01 22:12:47 +0500

    review and review pages added

--- frontend/app/src/Components/Review/Review.jsx
+++ frontend/app/src/Components/Review/Review.jsx
@@ -6,49 +6,58 @@ import { faStar } from '@fortawesome/free-regular-svg-icons';
 import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 
 const Review = memo(
-  ({
-    reviewerName = 'username',
-    reviewText = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, aliquid nesciunt a ',
-    reviewScore = 2,
-    reviewerImg = plimg,
-  }) => {
-    // const { reviewerName, reviewText, reviewScore, reviewerImg } = props;
-    reviewScore = reviewScore > 5 ? 5 : reviewScore;
+  ({ reviewerName, reviewText, reviewScore, reviewerImg }) => {
+    const defaultData = {
+      reviewerName: 'username',
+      reviewText:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, aliquid nesciunt a ',
+      reviewScore: 0,
+      reviewerImg: plimg,
+    };
+
+    const {
+      reviewerName: name = defaultData.reviewerName,
+      reviewText: text = defaultData.reviewText,
+      reviewScore: score = defaultData.reviewScore,
+      reviewerImg: img = defaultData.reviewerImg,
+    } = {
+      reviewerName,
+      reviewText,
+      reviewScore,
+      reviewerImg,
+    };
+
+    const stars = new Array(score).fill(null).map(() => (
+      <FontAwesomeIcon
+        className='icon-star'
+        icon={faStarSolid}
+        // style={{ color: '#ffc800', margin: '0 0.07rem', fontSize: '0.8rem' }}
+      />
+    ));
+    const emptyStars = new Array(5 - score).fill(null).map(() => (
+      <FontAwesomeIcon
+        className='icon-star'
+        icon={faStar}
+        // style={{ color: '#ffc800', margin: '0 0.07rem', fontSize: '0.8rem' }}
+      />
+    ));
+
     return (
       <div className='single-review'>
         <div className='reviewer-imCon'>
           <img
             className='reviewer-img'
-            style={{ width: '100%', objectFit: 'cover' }}
-            src={reviewerImg}
+            // style={{ width: '100%', objectFit: 'cover', objectFit: 'cover' }}
+            src={img}
             alt=''
           />
         </div>
-        <h4 className='reviewer-name'>{reviewerName}</h4>
-        {/* <div className='review-score'>{reviewScore}</div> */}
+        <h4 className='reviewer-name'>{name}</h4>
         <div className='review-stars'>
-          {new Array(reviewScore).fill(null).map(() => (
-            <FontAwesomeIcon
-              icon={faStarSolid}
-              style={{
-                color: '#ffc800',
-                margin: '0 0.07rem',
-                fontSize: '0.8rem',
-              }}
-            />
-          ))}
-          {new Array(5 - reviewScore).fill(null).map(() => (
-            <FontAwesomeIcon
-              icon={faStar}
-              style={{
-                color: '#ffc800',
-                margin: '0 0.07rem',
-                fontSize: '0.8rem',
-              }}
-            />
-          ))}
+          {stars}
+          {emptyStars}
         </div>
-        <h4 className='review-text'>{reviewText}</h4>
+        <h4 className='review-text'>{text}</h4>
       </div>
     );
   }
