cd88a44a951f074420eb4269907cff64953c0d6e
HasnainMubeen <113686598+HasnainMubeen@users.noreply.github.com>
2023-04-02 18:17:43 +0500

    Updated addToWishlist function

--- backend/controllers/wishlistController.js
+++ backend/controllers/wishlistController.js
@@ -1,1 +1,28 @@
+const Post = require("../models/posts");
 
+const addToWishlist = async (req, res) => {
+    const postId = req.body.postId;
+    const userId = req.body.userId;
+
+    try {
+        const post = await Post.findById(postId);
+        const wishlist = await Wishlist.findOne({ userId: userId });
+
+        if (wishlist){
+            wishlist.items.push(post);
+            await wishlist.save();
+        } else {
+            const newWishlist = new Wishlist({
+                userId: userId,
+                items: [post]
+            });
+            await newWishlist.save();
+        }
+        res.status(200).send("Added to Wishlist Successfully");
+    }catch(err){
+        res.status(500).send("Error");
+    }
+};
+
+
+module.exports = {addToWishlist}
