8439cace64c5bbf9e39c15016cf2f157b81aba5e
Abdullah-E <abdullahehsan4242@gmail.com>
2023-04-02 22:47:05 +0500

    spelling mistakes

--- backend/routes/wishlistRoutes.js
+++ backend/routes/wishlistRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {addToWishlist, removefromwishlist} = require('../controllers/wishlistController')
+const {addToWishlist, removeFromWishlist} = require('../controllers/wishlistController')
 
 router.route('/addtowishlist/product/:postId/user/:email').get(addToWishlist)
-router.route('/removefromwishlist/product/:postId/user/:email').get(removefromwishlist)
+router.route('/removefromwishlist/product/:postId/user/:email').get(removeFromWishlist)
 
 module.exports = router
