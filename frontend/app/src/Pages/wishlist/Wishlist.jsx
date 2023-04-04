f1de34439164aa6608e2da58a1e735190538806a
talha-husnain <92517278+talha-husnain@users.noreply.github.com>
2023-04-05 02:57:32 +0500

    Add files via upload

---
+++ frontend/app/src/Pages/wishlist/Wishlist.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import Wishitem from './Wishitem';
+import img from './img.jpg';
+import './wish.css';
+const Wishlist = () => {
+    const location = useLocation();
+
+    const [wishlist, setwishlist] = useState([]);
+    useEffect(() => {
+        console.log(location.state, 'test');
+        if (location.state) {
+          setwishlist(location.state.wish);
+          console.log(wishlist);
+        }
+      }, [location.state]);
+
+  return (
+    <div className='products'>
+      {wishlist.map((wish) => (
+        <Wishitem
+          id={wish._id}
+          title={wish.title}
+          image={img}
+          price={wish.price}
+          // status={product.status}
+        />
+      ))}
+    </div>  )
+}
+
+export default Wishlist
