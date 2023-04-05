925f0325d5980622f5b1f93aca5374cf20c1e146
Abdul Moez <24100244@lums.edu.pk>
2023-04-05 11:05:41 +0500

    commit abdullah changes on moez

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -1,23 +1,18 @@
 const mongoose = require('mongoose');
 
-
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const posts = new Schema({
-    title: {type: String, required:true},
-    description: {type:String, required:true},
-    price: {type:Number, required:true},
-    status: {type:Boolean, required:true,default:false},
-    sold_date: {type:Date, default:null},
-    img_URL: {type:[String], required:true},
-    date_created: {type:Date ,required:true, default:Date.now},
-    tags: {type:[String] ,  required:true, default: []},
-    flags: {type:Number, default:0},
-    user: {type: Schema.Types.ObjectId, required:true },
-
-    
-
-
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  status: { type: Boolean, required: true, default: false },
+  sold_date: { type: Date, default: null },
+  img_URL: { type: [String], required: true },
+  date_created: { type: Date, required: true, default: Date.now },
+  tags: { type: [String], required: true, default: [] },
+  flags: { type: Number, default: 0 },
+  user: { type: Schema.Types.ObjectId, required: true },
 });
 
 // posts.pre('save', function (next) {
