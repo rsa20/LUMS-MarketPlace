447d0049826c05530ce1def074ca7d52f155dacf
Abdul Moez <24100244@lums.edu.pk>
2023-04-14 01:02:05 +0500

    addpost images settled and view post inhomesettled

--- frontend/app/src/Pages/UploadImg/UploadImage.jsx
+++ frontend/app/src/Pages/UploadImg/UploadImage.jsx
@@ -1,59 +1,82 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-export default function Uploadimage() { 
-   const [values, setValues] = useState({
-      imagePreviewUrl: "", 
-      picFile: null
-   })
-   let fileInput = React.createRef(); 
+export default function Uploadimage() {
+  const [values, setValues] = useState({
+    imagePreviewUrls: [], // An array to store multiple image preview URLs
+    picFiles: [], // An array to store multiple files
+  });
 
-   const editProfilePic = () => {
-      fileInput.current.click();
-   } 
+  let fileInput = React.createRef();
 
-   const handleImageChange = e => {
-      e.preventDefault();
+  const editProfilePic = () => {
+    fileInput.current.click();
+  };
+
+  const handleImageChange = (e) => {
+    e.preventDefault();
+    const files = e.target.files;
+    let urls = [];
+    for (let i = 0; i < files.length; i++) {
       let reader = new FileReader();
-      let inFile = e.target.files[0];
       reader.onloadend = () => {
-         setValues({...values, 
-            picFile: inFile, 
-            imagePreviewUrl: reader.result
-         })
+        urls.push(reader.result);
+        if (urls.length === files.length) {
+          // When all URLs have been generated, update state
+          setValues({
+            ...values,
+            picFiles: [...values.picFiles, ...files],
+            imagePreviewUrls: [...values.imagePreviewUrls, ...urls],
+          });
+        }
       };
-      reader.readAsDataURL(inFile);
-   };
+      reader.readAsDataURL(files[i]);
+    }
+  };
 
-   const handleSubmit = async() => {
-      // Create a FormData object to store the file object
-      const form_data = new FormData();
-      form_data.append('file', values.picFile);
-
-      // Send the form data to the API endpoint using axios
-      try {
-         const response = await axios.post('api/cloudinary/upload', form_data);
-         console.log(response);
-      } catch (error) {
-         alert("Error occurred while uploading picture, try uploading a smaller image size or try again later.");
-         console.error(error);
+  const handleSubmit = async () => {
+    console.log(values.picFiles);
+    try {
+      const formDataArray = [];
+      for (let i = 0; i < values.picFiles.length; i++) {
+        const formData = new FormData();
+        formData.append('file', values.picFiles[i]);
+        formDataArray.push(formData);
       }
-   }
 
-   return(
-      <div>
-         <div onClick={() => editProfilePic()}>
-            <input 
-               type="file"
-               accept="image/*"
-               onChange={handleImageChange}
-               ref={fileInput} />
-            <img 
+      const responses = await Promise.all(
+        formDataArray.map((formData) =>
+          axios.post('api/cloudinary/upload', formData)
+        )
+      );
+      console.log(responses);
+    } catch (error) {
+      alert(
+        'Error occurred while uploading pictures, try uploading a smaller image size or try again later.'
+      );
+      console.error(error);
+    }
+  };
+
+  const imagessss = values.imagePreviewUrls.map((url) => {
+    return <img src={url} alt='...' style={{ objectFit: 'cover' }} />;
+  });
+  return (
+    <div>
+      <div onClick={() => editProfilePic()}>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleImageChange}
+          ref={fileInput}
+        />
+        {imagessss}
+        {/* <img 
                src={values.imagePreviewUrl}
                alt="..."
-               style={{objectFit: 'cover'}}/>
-         </div> 
-         <button onClick={handleSubmit}>Submit</button>
+               style={{objectFit: 'cover'}}/> */}
       </div>
-   )
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
